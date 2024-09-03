c1fdf0503c8097e6c2f829220d705e247ed76a0b
Antony Gonzales Rojas <128429264+tonyfgr@users.noreply.github.com>
2024-09-03 10:05:29 -0500

    Update manage.sh 1

--- ApiCsharp/manage.sh
+++ ApiCsharp/manage.sh
@@ -2,7 +2,7 @@
 WORKDIR="/tmp/mymicroservice"
 
 # Clonar el repositorio
-git clone https://github.com/MariaCutipa/ExamenDAEA.git $WORKDIR
+git clone https://github.com/tonyfgr/exam1daea.git $WORKDIR
 
 # Cambiar al directorio del repositorio
 cd $WORKDIR/ApiCsharp
